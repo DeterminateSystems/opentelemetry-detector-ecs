fbfbcef1f04067abaf30a00d84d01bee48f5f9ae
Graham Christensen <graham@grahamc.com>
2026-08-02 18:39:42 -0400

    Commit the lockfile

---
+++ flake.nix
@@ -0,0 +1,143 @@
+{
+  description = "An OpenTelemetry resource detector for Amazon ECS";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/*";
+
+    fenix.url = "https://flakehub.com/f/nix-community/fenix/*";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+
+    crane.url = "https://flakehub.com/f/ipetkov/crane/*";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      fenix,
+      crane,
+    }:
+    let
+      inherit (nixpkgs) lib;
+
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forEachSystem =
+        f:
+        lib.genAttrs systems (
+          system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+
+            toolchain =
+              with fenix.packages.${system};
+              combine [
+                stable.cargo
+                stable.clippy
+                stable.rust-src
+                stable.rustc
+                stable.rustfmt
+              ];
+
+            craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+
+            src = craneLib.cleanCargoSource ./.;
+
+            common = {
+              inherit src;
+              strictDeps = true;
+            };
+
+            cargoArtifacts = craneLib.buildDepsOnly common;
+          in
+          f {
+            inherit
+              pkgs
+              toolchain
+              craneLib
+              common
+              cargoArtifacts
+              ;
+          }
+        );
+    in
+    {
+      checks = forEachSystem (
+        {
+          pkgs,
+          craneLib,
+          common,
+          cargoArtifacts,
+          ...
+        }:
+        {
+          build = craneLib.cargoBuild (common // { inherit cargoArtifacts; });
+
+          clippy = craneLib.cargoClippy (
+            common
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
+
+          doc = craneLib.cargoDoc (
+            common
+            // {
+              inherit cargoArtifacts;
+              env.RUSTDOCFLAGS = "--deny warnings";
+            }
+          );
+
+          rustfmt = craneLib.cargoFmt { inherit (common) src; };
+
+          test = craneLib.cargoTest (common // { inherit cargoArtifacts; });
+
+          editorconfig = pkgs.runCommand "check-editorconfig" { } ''
+            ${lib.getExe pkgs.eclint} ${self}
+            touch $out
+          '';
+
+          nixfmt = pkgs.runCommand "check-nixfmt" { } ''
+            ${lib.getExe pkgs.nixfmt} --check ${self}
+            touch $out
+          '';
+
+          typos = pkgs.runCommand "check-typos" { } ''
+            ${lib.getExe pkgs.typos} ${self}
+            touch $out
+          '';
+        }
+      );
+
+      devShells = forEachSystem (
+        { pkgs, toolchain, ... }:
+        {
+          default = pkgs.mkShell {
+            name = "opentelemetry-detector-ecs";
+
+            packages = [
+              toolchain
+
+              pkgs.cargo-audit
+              pkgs.cargo-machete
+              pkgs.cargo-outdated
+              pkgs.eclint
+              pkgs.nixfmt
+              pkgs.rust-analyzer
+              pkgs.typos
+            ];
+
+            env.RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+          };
+        }
+      );
+
+      formatter = forEachSystem ({ pkgs, ... }: pkgs.nixfmt);
+    };
+}
